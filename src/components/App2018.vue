e83fbc7da5f563c1b6f9402dbed2e55303774052
Sarah Drasner <sarah.drasner@gmail.com>
2020-07-11 23:23:20 -0600

    add in jamstack conf

--- src/components/App2018.vue
+++ src/components/App2018.vue
@@ -11,10 +11,17 @@
     <app-streetlamp2 x="100" y="3280" />
 
     <app-drop />
-    <app-text x="0" y="3950" num="10">
+    <app-text x="0" y="3950" num="12">
       <template v-slot:date>Mar 20, 2018</template>
       <template v-slot:event>Launched Netlify Drop</template>
       <template v-slot:event2>a simple drag-n-drop way to launch sites</template>
+    </app-text>
+
+    <app-jamstack1 />
+    <app-tree x="600" y="4000" />
+    <app-text x="1000" y="4200" num="12">
+      <template v-slot:date>Oct 10, 2018</template>
+      <template v-slot:event>World's First Jamstack Conf</template>
     </app-text>
   </g>
 </template>
@@ -28,6 +35,7 @@ import AppStreetlamp2 from "@/components/AppStreetlamp2.vue";
 
 //unique units
 import AppDrop from "@/components/AppDrop.vue";
+import AppJamstack1 from "@/components/AppJamstack1.vue";
 import AppFunctions from "@/components/AppFunctions.vue";
 
 export default {
@@ -37,6 +45,7 @@ export default {
     AppBush,
     AppStreetlamp2,
     AppDrop: () => import("@/components/AppDrop.vue"),
+    AppJamstack1: () => import("@/components/AppJamstack1.vue"),
     AppFunctions: () => import("@/components/AppFunctions.vue")
   }
 };
