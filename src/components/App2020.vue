0226074e3e1f7ada0924bfc16ada0a6a3cb57d92
Sarah Drasner <sarah.drasner@gmail.com>
2020-07-17 23:40:41 -0600

    add in virtual

--- src/components/App2020.vue
+++ src/components/App2020.vue
@@ -1,15 +1,18 @@
 <template>
   <g>
+    <app-virtual />
     <app-build-plugins />
   </g>
 </template>
 
 <script>
 import AppBuildPlugins from "@/components/AppBuildPlugins.vue";
+import AppVirtual from "@/components/AppVirtual.vue";
 
 export default {
   components: {
-    AppBuildPlugins: () => import("@/components/AppBuildPlugins.vue")
+    AppBuildPlugins: () => import("@/components/AppBuildPlugins.vue"),
+    AppVirtual: () => import("@/components/AppVirtual.vue")
   }
 };
 </script>
