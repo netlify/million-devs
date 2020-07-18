da1bff7eb283b8b6dbce94984029a3ebc306c143
Sarah Drasner <sarah.drasner@gmail.com>
2020-07-18 08:47:42 -0600

    lamp and bush placements

--- src/components/App2020.vue
+++ src/components/App2020.vue
@@ -1,13 +1,22 @@
 <template>
   <g>
+    <app-tree x="950" y="5700" />
+    <app-tree x="1000" y="5750" />
+    <app-bush x="1050" y="5900" />
+    <app-streetlamp2 x="700" y="6000" />
+
     <app-virtual />
     <app-text x="1400" y="6500" num="19">
       <template v-slot:date>May 27, 2020</template>
       <template v-slot:event>Jamstack Conf Virtual</template>
     </app-text>
 
+    <app-streetlamp2 x="1300" y="6600" />
+    <app-tree x="800" y="6250" />
+    <app-tree x="850" y="6200" />
+
     <app-build-plugins />
-    <app-text x="1400" y="6500" num="20">
+    <app-text x="200" y="6700" num="20">
       <template v-slot:date>May 27, 2020</template>
       <template v-slot:event>Launched Build Plugins</template>
     </app-text>
