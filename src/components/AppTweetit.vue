0efc22afee0593e8551b8664a68fe06615b3bc04
Zach Leatherman <zachleatherman@gmail.com>
2020-07-30 18:21:40 -0500

    Copy typo in tweet text

--- src/components/AppTweetit.vue
+++ src/components/AppTweetit.vue
@@ -12,7 +12,7 @@ export default {
       let link = this.$store.getters.tweetPreviewLink;
       let text = `Netlify has onboarded 1 Million Developers and I was #${this.user.number}!
 
-Find our your number at https://million-devs.netlify.com/
+Find out your number at https://million-devs.netlify.com/
 
 ${link}`;
       let url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&via=Netlify`;
